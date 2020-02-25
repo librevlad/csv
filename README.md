25c6cc64d8904e9d464c078d5baa2382ca3d172a
librevlad <librevlad@gmail.com>
2020-02-25 22:27:02 +0200

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# CSV helper
+
+A tiny function to read CSV files.
+
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require librevlad/csv
+```
+
+## Usage
+```php
+$data = floadcsv( $file, $headings = false, $delimiter = ',', $limit = null )
+```
+
+## License
+
+The MIT License (MIT).
---
+++ helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+function floadcsv( $file, $headings = false, $delimiter = ',', $limit = null ) {
+
+    $fp    = fopen( $file, 'r' );
+    $lines = [];
+
+    if ( $headings ) {
+        $h = fgetcsv( $fp, 0, $delimiter );
+    }
+
+    $taken = 0;
+    while ( ( $taken < $limit ) && ( $l = fgetcsv( $fp, 0, $delimiter ) ) ) {
+        $taken ++;
+        if ( ! $headings ) {
+            $result = $l;
+        } else {
+            $result = [];
+            foreach ( array_slice( $l, 0, count( $h ) ) as $i => $cell ) {
+                $result[ $h[ $i ] ] = $cell;
+            }
+        }
+        $lines [] = $result;
+    }
+
+    fclose( $fp );
+
+    return $lines;
+
+}
