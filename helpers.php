32f2a00caa117c007257d937a8b08694319c8309
librevlad <librevlad@gmail.com>
2022-08-01 13:50:44 +0300

    wip

--- helpers.php
+++ helpers.php
@@ -2,32 +2,57 @@
 
 function floadcsv( $file, $headings = false, $delimiter = ',', $limit = null ) {
 
-    $fp    = fopen( $file, 'r' );
-    $lines = [];
+	$fp    = fopen( $file, 'r' );
+	$lines = [];
 
-    if ( is_array( $headings ) ) {
-        $h = $headings;
-    } elseif ( $headings == true ) {
-        $h = fgetcsv( $fp, 0, $delimiter );
-    }
+	if ( is_array( $headings ) ) {
+		$h = $headings;
+	} elseif ( $headings == true ) {
+		$h = fgetcsv( $fp, 0, $delimiter );
+	}
 
-    $taken = 0;
-    $limit = $limit ? $limit : INF;
-    while ( ( $taken < $limit ) && ( $l = fgetcsv( $fp, 0, $delimiter ) ) ) {
-        $taken ++;
-        if ( ! $headings ) {
-            $result = $l;
-        } else {
-            $result = [];
-            foreach ( array_slice( $l, 0, count( $h ) ) as $i => $cell ) {
-                $result[ $h[ $i ] ] = $cell;
-            }
-        }
-        $lines [] = $result;
-    }
+	$taken = 0;
+	$limit = $limit ? $limit : INF;
+	while ( ( $taken < $limit ) && ( $l = fgetcsv( $fp, 0, $delimiter ) ) ) {
+		$taken ++;
+		if ( ! $headings ) {
+			$result = $l;
+		} else {
+			$result = [];
+			foreach ( array_slice( $l, 0, count( $h ) ) as $i => $cell ) {
+				$result[ $h[ $i ] ] = $cell;
+			}
+		}
+		$lines [] = $result;
+	}
 
-    fclose( $fp );
+	fclose( $fp );
 
-    return $lines;
+	return $lines;
+
+}
+
+function fsavecsv( $file, $data, $delimiter = ',' ) {
+
+	$heading = [];
+	if ( count( $data ) ) {
+		$heading = array_keys( array_values( $data )[ 0 ] );
+	}
+
+	$content = implode( $delimiter, $heading );
+
+	foreach ( $data as $row ) {
+
+		$line = [];
+
+		foreach ( $heading as $th ) {
+			$line [] = ( $row[ $th ] ?? '' );
+		}
+
+		$content .= PHP_EOL . implode( $delimiter, $line );
+
+	}
+
+	file_put_contents( $file, $content);
 
 }
--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -6,8 +6,28 @@ use PHPUnit\Framework\TestCase;
 
 class BasicTest extends TestCase {
 
-    public function testBasicApi(  ) {
-        $this->assertEquals(1,1);
-    }
+	public function testBasicApi() {
+		$this->assertEquals( 1, 1 );
+
+		$data = [
+			[
+				'first_name' => 'Petro',
+				'patronymic' => 'Oleksiiovich',
+				'last_name'  => 'Poroshenko',
+			],
+			[
+				'last_name'  => 'Kravchuk',
+				'first_name' => 'Leonid',
+			],
+			[
+				'first_name' => 'Leonid',
+				'patronymic' => 'Danylovych',
+				'last_name'  => 'Kuchma',
+			],
+		];
+
+		fsavecsv( 'presidents.csv', $data );
+
+	}
 
 }
